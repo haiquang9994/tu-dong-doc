921f1c58823656089931f61b60ca9c3d31919b04
Nguyễn Hải Quang <haiquang9994@gmail.com>
2022-05-30 18:58:14 +0700

    home.html

--- pages/views.py
+++ pages/views.py
@@ -11,17 +11,16 @@ import base64
 import hashlib
 from pydub import AudioSegment
 
+
 class HttpResponseMethodNotAllowed(HttpResponse):
     status_code = HTTPStatus.METHOD_NOT_ALLOWED
 
+
 def homePageView(request):
-    context = {
-        "first_name": "Anjaneyulu",
-        "last_name": "Batta",
-        "address": "Hyderabad, India"
-    }
-    template_name = "home2.html"
+    context = {}
+    template_name = "home.html"
     return render(request, template_name, context)
+
 
 def aboutPageView(request):
     context = {
@@ -31,10 +30,12 @@ def aboutPageView(request):
     template_name = "about.html"
     return render(request, template_name, context)
 
+
 def md5(text):
     m = hashlib.md5()
     m.update(text.encode())
     return m.hexdigest()
+
 
 @csrf_exempt
 def textToSpeech(request):
@@ -44,13 +45,15 @@ def textToSpeech(request):
         text = re.sub(r'\.', '. ', text)
         text = re.sub(r'\.\ \ ', '. ', text)
         text = re.sub(r'\.\ +\"', '."', text)
-        rows = [r for r in re.compile("\.(\ )|\-").split(text.strip()) if (r != '' and r != None and r.strip() != "")]
+        rows = [r for r in re.compile(
+            "\.(\ )|\-").split(text.strip()) if (r != '' and r != None and r.strip() != "")]
         final_text = '. '.join(rows).strip()
         output = gTTS(final_text, lang="vi", slow=False)
         filename = 'mp3/' + md5(text) + '.mp3'
         output.save(filename)
         sound = AudioSegment.from_mp3(filename)
-        faster_sound = sound._spawn(sound.raw_data, overrides={"frame_rate": int(sound.frame_rate * 1.2)})
+        faster_sound = sound._spawn(sound.raw_data, overrides={
+                                    "frame_rate": int(sound.frame_rate * 1.2)})
         faster_sound.export(filename, format="mp3")
         response_data = {}
         response_data['text'] = final_text
@@ -59,11 +62,13 @@ def textToSpeech(request):
     else:
         return HttpResponseMethodNotAllowed()
 
+
 def GlobalToBase64(file):
     with open(file, 'rb') as fileObj:
         image_data = fileObj.read()
         base64_data = base64.b64encode(image_data)
         return base64_data.decode()
+
 
 @csrf_exempt
 def uploadFilePageView(request):
